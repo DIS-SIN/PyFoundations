636d60e760d967a5184e38d77e6735dfe051dcc2
Sinan Baltacioglu <siphon@gmail.com>
2019-02-07 16:45:05 -0500

    Francais, OUI! Le React c'est bon!

--- static/pyfoundations/js/Greeting.jsx
+++ static/pyfoundations/js/Greeting.jsx
@@ -1,26 +1,67 @@
 import React, { Component } from "react";
 
+import LocalizedStrings from 'react-localization';
+
+let strings = new LocalizedStrings({
+    en: {
+        welcome: "Hello!",
+        knownmessage: "Welcome back.",
+        unknownmessage: "Please Sign Up.",
+        changelanguage: "Français"
+    },
+    fr: {
+        welcome: "Bonjour!",
+        knownmessage: "Bienvenue.",
+        unknownmessage: "Inscrivez vous s'il vous plait.",
+        changelanguage: "English"
+    }
+});
+
 const GuestGreeting = (props) => { 
     return (
-        <h1>Please sign up.</h1>
+        <h1>{strings.welcome} {strings.unknownmessage}</h1>
     );
 }
 const UserGreeting = (props) => { 
     return (
-        <h1>Welcome back!</h1>
+        <h1>{strings.welcome} {strings.knownmessage}</h1>
     );
 }
 
 class Greeting extends React.Component {
     constructor(props) {
         super(props);
+        //this.state = { currentLang: "en" };
     }    
+
+    toggleCanadianOfficialLanguage = () => {
+        if (strings.getLanguage() == "en") {
+            strings.setLanguage('fr');
+            this.setState({});
+        } else {
+            strings.setLanguage('en');
+            this.setState({});
+        }
+    }
+
     render() {
         const { isLoggedIn }  = this.props;
+        //const { currentLang } = this.state;
+        let greetingFragment = null; 
         if (isLoggedIn) {
-            return <UserGreeting />;
+            greetingFragment = <UserGreeting />;
+        } else {
+            greetingFragment = <GuestGreeting />;
         }
-        return <GuestGreeting />;
+
+        return (
+            <div className="greetingBlock">
+                {greetingFragment}
+                <button onClick={this.toggleCanadianOfficialLanguage}>
+                    {strings.changelanguage}
+                </button>
+            </div>
+        )
     }
 }
 
