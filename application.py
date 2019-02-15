7d02bb352499ed998d64c676ce2ee12f336ec81e
Matthew Clements <matthew.sta.clements@gmail.com>
2019-02-15 11:17:02 -0500

    update foundations application to connect to db with sqlalchemy

--- application.py
+++ application.py
@@ -1,12 +1,44 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, jsonify
 import os
 from code import pyfoundations
+from code.blueprint.api import (
+    learning_points_bp,
+    tags_bp,
+    learning_resources_bp,
+    users_bp,
+)
+
+
 def create_app():
     app = Flask(__name__)
+
+    app.config.from_mapping(
+        DATABASE="postgresql+psycopg2://postgres:password@localhost:5432/foundationstest"
+    )
+
     app.register_blueprint(pyfoundations.bp)
-    @app.route('/')
+    app.register_blueprint(learning_points_bp.bp)
+    app.register_blueprint(tags_bp.bp)
+    app.register_blueprint(learning_resources_bp.bp)
+    app.register_blueprint(users_bp.bp)
+
+    @app.route("/")
     def index():
-        return redirect(url_for('pyfoundations.render_home'))
+        ret_val = {"experience": {"id": 1, "name": "Hey"}}
+        return jsonify(ret_val)
+
+    @app.route("/about")
+    def about():
+        ret_val = {
+            "details": {
+                "title": "This is the about page",
+                "description": "my about page description",
+            }
+        }
+        return jsonify(ret_val)
+
     return app
+
+
 app = create_app()
-app.run(host = '0.0.0.0', port=5054)
+app.run(host="0.0.0.0", port=5054)
