4d608bde41b8430aa8a7878338db4d9fb2da6cf1
Matthew Clements <matthew.sta.clements@gmail.com>
2019-02-19 15:38:48 -0500

    add unimplemented methods, update return values
    add delete, insert, update functionality
    for the return values: added "api_return" and "api_data" into array at first index

--- code/blueprint/api/blueprint_factory.py
+++ code/blueprint/api/blueprint_factory.py
@@ -1,30 +1,84 @@
-from flask import jsonify
+from flask import jsonify, request
 from code.json_encoder import AlchemyEncoder
-from code.database.middleware import get_models_by_id, get_all_of_model
+from code.database.middleware import (
+    get_models_by_id,
+    get_all_of_model,
+    update_model_by_id,
+    insert_model,
+    delete_model_by_id,
+)
 
-# encoder that takes sqlalchemy class, converts to json object
 json_en = AlchemyEncoder
 
 
 def view_one(model, id):
-    # Get all models that match the id from the db
-    db_return = get_models_by_id(model, id)
+    """Return a jsonified version of a model in a db that matches id
 
-    # If we get one, continue, otherwise return a failure message
+    first retreive model from the db
+    if the return size is 0, or more than 1, return no data with a fail
+    if the return size is 1, continue
+        using an implimentation of flask.json.JSONEncoder, process model into json
+        return jsonified model with a success
+    """
+    db_return = get_models_by_id(model, id)
     if len(db_return) == 1:
         return jsonify(
-            json_en.default(json_en, db_return[0]), {"api_return": "success"}
+            [
+                {
+                    "api_data": json_en.default(json_en, db_return[0]),
+                    "api_return": "success",
+                }
+            ]
         )
-    return jsonify({"api_return": "failure"})
+    return jsonify([{"api_return": "failure"}])
 
 
 def view_all(model):
-    # Get all of the record of same type as model
+    """Return an array of jsonified versions of all models in a db
+
+    first retreive all models from the db
+    create an array to store jsonified version of each model
+    for each model returned by the db
+        using an implimentation of flask.json.JSONEncoder turn into json,
+        add to the array
+    return jsonified array with a success
+    (if there are no models in the db it returns empty array with api_return:success)
+    """
     db_return = get_all_of_model(model)
-
-    # array for constructing return value
     return_obj = []
-
     for obj in db_return:
         return_obj.append(json_en.default(json_en, obj))
-    return jsonify(return_obj, {"api_return": "success"})
+    return jsonify([{"api_data": return_obj, "api_return": "success"}])
+
+
+def update_one(model, id, object_name):
+    request_data = request.get_json()
+    if request_data[object_name] is not None:
+        db_status = update_model_by_id(model, id, request_data[object_name])
+        if db_status:
+            return jsonify([{"api_return": "success"}])
+    return jsonify([{"api_return": "failure"}])
+
+
+def insert_one(model, object_name):
+    request_data = request.get_json()
+    if request_data[object_name] is not None:
+        new_model = model(**request_data[object_name])
+        db_status = insert_model(new_model)
+        if db_status:
+            return jsonify([{"api_return": "success"}])
+    return jsonify([{"api_return": "failure"}])
+
+
+def __can_delete__():
+    """Returns whether or not the user for the current session has permission to delete an object
+    """
+    return False  # TODO Real value
+
+
+def delete_one(model, id):
+    if __can_delete__():
+        db_status = delete_model_by_id(model, id)
+        if db_status:
+            return jsonify([{"api_return": "success"}])
+    return jsonify([{"api_return": "failure"}])
