e662d4027853b592aed2ee2a9ad423844e93f4f0
Sinan Baltacioglu <siphon@gmail.com>
2019-02-22 14:45:55 -0500

    View Layout Stub

---
+++ static/js/components/templates/DOLViewLayout.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { connect } from "react-redux";
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import DOLHeader from "../organisms/DOLHeader";
+import DOLFooter from "../organisms/DOLFooter";
+import DOLPrimaryActionPanel from "../organisms/DOLPrimaryActionPanel";
+
+const mapStateToProps = state => {
+    return {
+        literals: state.literals
+    };
+};
+
+const styles = theme => ({
+    fabButton: {
+        position: 'fixed',
+        zIndex: 1,
+        bottom: 35,
+        left: 'auto',
+        right: 35,
+        margin: '0 auto',
+    },
+})
+
+class DOLViewLayout extends React.Component {
+
+    render() {
+        const { literals, classes, match, content, location } = this.props;
+
+        return (
+            <React.Fragment>
+                <DOLHeader location={location} />
+                <CssBaseline />
+                {content}
+                <DOLPrimaryActionPanel />
+                <DOLFooter />
+            </React.Fragment>
+        );
+    }
+}
+
+DOLViewLayout.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+// connect redux state
+//export default connect(mapStateToProps)(withStyles(styles)(DOLLayout));
+export default withRouter(connect(mapStateToProps)(withStyles(styles)(DOLViewLayout)));
+
