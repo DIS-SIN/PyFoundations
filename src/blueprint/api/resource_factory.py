d75954a04785326973b905a1a4fbddcb8253dec0
Omar Nasr <omardev9898@gmail.com>
2019-02-24 17:59:12 -0500

    Config best practices, CLI tools

--- src/blueprint/api/resource_factory.py
+++ src/blueprint/api/resource_factory.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
-from code.json_encoder import AlchemyEncoder
-from code.database.middleware import (
+from src.json_encoder import AlchemyEncoder
+from src.database.middleware import (
     get_models_by_id,
     get_all_of_model,
     update_model_by_id,
@@ -17,7 +17,7 @@ def view_one(model, id):
     first retreive model from the db
     if the return size is 0, or more than 1, return no data with a fail
     if the return size is 1, continue
-        using an implimentation of flask.json.JSONEncoder, process model into json
+        using an implimentation of flask.json.JSONEnsrcr, process model into json
         return jsonified model with a success
     """
     db_return = get_models_by_id(model, id)
@@ -39,7 +39,7 @@ def view_all(model):
     first retreive all models from the db
     create an array to store jsonified version of each model
     for each model returned by the db
-        using an implimentation of flask.json.JSONEncoder turn into json,
+        using an implimentation of flask.json.JSONEnsrcr turn into json,
         add to the array
     return jsonified array with a success
     (if there are no models in the db it returns empty array with api_return:success)
