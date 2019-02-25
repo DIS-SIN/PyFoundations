a99ee98f8b83f6781c69b44d99dea2913b856afb
Sinan Baltacioglu <siphon@gmail.com>
2019-02-25 01:57:21 -0500

    Mobile Responsive View Layout

--- static/js/components/templates/DOLViewLayout.jsx
+++ static/js/components/templates/DOLViewLayout.jsx
@@ -7,7 +7,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import DOLHeader from "../organisms/DOLHeader";
 import DOLFooter from "../organisms/DOLFooter";
 import DOLPrimaryActionPanel from "../organisms/DOLPrimaryActionPanel";
-
+import Hidden from '@material-ui/core/Hidden';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -19,10 +19,13 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import NavigationIcon from '@material-ui/icons/Navigation';
 import DOLPageExploreEpisodes from "../views/DOLPageExploreEpisodes";
 import DOLEpisodes from "../organisms/DOLEpisodes";
 import ScrollToTop from "../atoms/ScrollToTop";
 import { Link } from 'react-router-dom';
+import { IconButton, Button, Fab } from "@material-ui/core";
 
 const mapStateToProps = state => {
     return {
@@ -50,14 +53,31 @@ const styles = theme => ({
         padding: 0,
         margin: 0,
     },
+    //appBar: {
+    //    zIndex: theme.zIndex.drawer + 1,
+    //},
     appBar: {
         zIndex: theme.zIndex.drawer + 1,
+        marginLeft: drawerWidth,
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+        },
     },
-    drawer: {
+
+    /*drawer: {
         width: drawerWidth,
         flexShrink: 0,
         color: 'white',
+    },*/
+    drawer: {
+        color: 'white',
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+            color: 'white',
+        },
     },
+
     drawerPaper: {
         width: drawerWidth,
         backgroundColor: '#333333',//'rgb(0,140,160)',
@@ -70,14 +90,57 @@ const styles = theme => ({
     toolbar: theme.mixins.toolbar,
     highContrast: {
         color: 'white',
-    }
+    },
+    menuButtonActions: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    fabButtonDrawer: {
+        position: 'fixed',
+        zIndex: 1,
+        bottom: 35,
+        left: 'auto',
+        right: 35,
+        margin: '0 auto',
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
 });
-
 class DOLViewLayout extends React.Component {
+    state = {
+        mobileOpen: false,
+    };
 
+    handleDrawerToggle = () => {
+        this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+    };
     render() {
-        const { literals, classes, match, content, location, fetchid } = this.props;
-
+        const { literals, classes, theme, match, content, location, fetchid } = this.props;
+        const drawer = (
+            <div>
+                <div className={classes.toolbar} />
+                <List>
+                    {['Back', 'Add to Profile', 'Rate Content', 'Related Content'].map((text, index) => (
+                        <ListItem component={Link} to={"/explore/" + fetchid} button key={text}>
+                            <ListItemIcon className={classes.highContrast}>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} classes={{ primary: classes.highContrast }} />
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {['Share Content', 'Comment', 'Like'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon className={classes.highContrast}>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} classes={{ primary: classes.highContrast }} />
+                        </ListItem>
+                    ))}
+                </List>
+            </div>
+        );
         return (
             <React.Fragment>
                 <ScrollToTop />
@@ -88,36 +151,51 @@ class DOLViewLayout extends React.Component {
                             <DOLHeader className={classes.appBar} location={location} />
                         </Toolbar>
                     </AppBar>
-                    <Drawer
-                        className={classes.drawer}
-                        variant="permanent"
-                        classes={{
-                            paper: classes.drawerPaper,
-                        }}
-                    >
-                        <div className={classes.toolbar} />
-                        <List>
-                            {['Back', 'Add to Profile', 'Rate Content', 'Related Content'].map((text, index) => (
-                                <ListItem component={Link} to={"/explore/" + fetchid} button key={text}>
-                                    <ListItemIcon className={classes.highContrast}>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                                    <ListItemText primary={text} classes={{ primary: classes.highContrast }} />
-                                </ListItem>
-                            ))}
-                        </List>
-                        <Divider />
-                        <List>
-                            {['Share Content', 'Comment', 'Like'].map((text, index) => (
-                                <ListItem button key={text}>
-                                    <ListItemIcon className={classes.highContrast}>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                                    <ListItemText primary={text} classes={{ primary: classes.highContrast }} />
-                                </ListItem>
-                            ))}
-                        </List>
-                    </Drawer>
+
+                    <nav className={classes.drawer}>
+                        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                        <Hidden smUp implementation="css">
+                            <Drawer
+                                container={this.props.container}
+                                variant="temporary"
+                                anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                                open={this.state.mobileOpen}
+                                onClose={this.handleDrawerToggle}
+                                classes={{
+                                    paper: classes.drawerPaper,
+                                }}
+                            >
+                                {drawer}
+                            </Drawer>
+                        </Hidden>
+                        <Hidden xsDown implementation="css">
+                            <Drawer
+                                classes={{
+                                    paper: classes.drawerPaper,
+                                }}
+                                variant="permanent"
+                                open
+                            >
+                                {drawer}
+                            </Drawer>
+                        </Hidden>
+                    </nav>
+
                     <main className={classes.content}>
                         <div className={classes.toolbar} />
+                        <div className={classes.menuButtonActions}>
+                            <DOLPrimaryActionPanel />
+                        </div>
+                        <Fab
+                            variant="extended"
+                            color="secondary"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerToggle}
+                            className={classes.fabButtonDrawer}>
+                            <NavigationIcon />
+                            {literals.common.actions}
+                        </Fab>
                         {content}
-                        <DOLPrimaryActionPanel />
                         <DOLFooter />
                     </main>
                 </div>
@@ -125,12 +203,23 @@ class DOLViewLayout extends React.Component {
         );
     }
 }
-
+/**
+ * <Button
+                            color="secondary"
+                            variant="contained"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerToggle}
+                            className={classes.menuButton}
+                        >
+                            <MenuIcon /> {literals.common.actions}
+                        </Button>
+ */
 DOLViewLayout.propTypes = {
     classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
 };
 
 // connect redux state
 //export default connect(mapStateToProps)(withStyles(styles)(DOLLayout));
-export default withRouter(connect(mapStateToProps)(withStyles(styles)(DOLViewLayout)));
+export default withRouter(connect(mapStateToProps)(withStyles(styles, { withTheme: true })(DOLViewLayout)));
 
