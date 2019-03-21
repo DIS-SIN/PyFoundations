50c081bac11de0f21b5d077110fb3bced42dba62
Abdu997 <aamou094@uottawa.ca>
2019-03-21 15:04:30 -0400

    - Specified utf-8 encoding to ftp callbacks

--- src/database/data/loaders/episodes.py
+++ src/database/data/loaders/episodes.py
@@ -4,8 +4,8 @@ from src.database.utils.crud import create_row
 from flask import g
 import os
 def load_episodes(app,file):
-    with open(os.path.join(app.static_folder,"data","episodes.json"), 'r') as f:
-        data = json.load(f)
+    with open(os.path.join(app.static_folder,"data","episodes.json"), 'r', encoding = "utf-8") as f:
+        data = json.load(f, encoding = 'utf-8')
     converter = EpisodeSchema()
     converter.session = g.db
     for episode in data:
